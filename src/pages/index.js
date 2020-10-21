80ef3d611ef870ab5ae9892408783a12ab93bda9
minamiriku <r.minami@leihauoli.com>
2020-10-21 12:57:08 +0900

    index.js編集

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,36 @@
 import React from "react"
-import { Link } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query allContentfulUser {
+      allContentfulUser {
+        nodes {
+          id
+          name
+          age
+        }
+      }
+    }
+  `)
+  return(
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
+    {data.allContentfulUser.nodes.map(({ id, headerImage, title, body, slug }) => (
+      <>
+        <h3 key={id}>
+          {title}
+        </h3>
+        <img src={headerImage} />
+        <p>{body}</p>
+        <span>{slug}</span>
+      </>
+      ))}
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
@@ -17,6 +39,6 @@ const IndexPage = () => (
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
-)
-
+  )
+}
 export default IndexPage
